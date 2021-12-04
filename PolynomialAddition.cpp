34da59048fe01a8c58c9d8797594a17058fb0b31
Sourav Bera <sauravberaiit@gmail.com>
2021-12-04 21:17:30 +0530

    Added Polynomial Addittion file

---
+++ PolynomialAddition.cpp
@@ -0,0 +1,21 @@
+struct Node
+{
+    int coeff;
+    int pow;
+    struct Node* next;
+
+    Node(int c, int p)
+    {
+        coeff= c;
+        pow= p;
+        next= NULL;
+    }
+};
+
+class Solution{
+    public:
+    Node* addPolynomial(Node *p1, Node *p2)
+    {
+        Node 
+    }
+}
